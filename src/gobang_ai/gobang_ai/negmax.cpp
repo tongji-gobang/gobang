cdd9008cf4b16cd8af09b46783908d172cfe4ce7
Alivers <321204342@qq.com>
2019-03-10 14:39:57 +0800

    更新negmax

--- src/gobang_ai/gobang_ai/negmax.cpp
+++ src/gobang_ai/gobang_ai/negmax.cpp
@@ -18,6 +18,17 @@ auto Pruning( int depth, int alpha, int beta, int role )
 
 struct point board::Deeping( PointSet& candidates, int role, int depth )
 {
+    // 教程中加了超时计时器，暂时忽略
+    int MaxScore;
+    for ( int i = 2; i <= depth; i += 2 )
+    {
+        // 初始调用
+        MaxScore = Negmax(candidates, role, depth, INT_MIN, INT_MAX );
+        if ( MaxScore >=  FIVE )    // 搜索到能赢的棋
+            break;
+    }
+
+    
 
 }
 struct point board::DepthSearchAll( int role, int depth )
@@ -25,8 +36,8 @@ struct point board::DepthSearchAll( int role, int depth )
     // 教程源码中写法： role = role || R.com --- 这句恒为真，不知何意
     role = COM;
 
-    auto candidates = gen( role );  // vector<point>
+    PointSet candidates = gen( role );  // vector<point>
 
-    return deeping( candidates, role, depth );
+    return Deeping( candidates, role, depth );
 }
 
